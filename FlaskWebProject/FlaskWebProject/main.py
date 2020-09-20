b6016118ce33d928175da8affde1c1fe5199ed9a
Tomer <tomerkeren42@gmail.com>
2020-09-20 14:02:47 +0300

    sync with web-hosted on GAE

--- FlaskWebProject/FlaskWebProject/main.py
+++ FlaskWebProject/FlaskWebProject/main.py
@@ -4,15 +4,13 @@ The flask application package.
 
 from datetime import datetime
 from flask import render_template, Flask, request, redirect
-# from FlaskWebProject import app
 from os import environ
-
 # from google.cloud import storage
 
 # __init__ file
-
 # the Flask APP and the Google Cloud storage initiation
 app = Flask(__name__)
+
 # CLOUD_STORAGE_BUCKET = environ['CLOUD_STORAGE_BUCKET']
 
 # views.py file
@@ -59,15 +57,15 @@ def help():
 # runserver file
 
 if __name__ == '__main__':
+    # For local enviornment:
+
     #HOST = environ.get('SERVER_HOST', 'localhost')
     #try:
     #    PORT = int(environ.get('SERVER_PORT', 5555))  
     #except ValueError:
     #    PORT = 5555
-    #print("HOST is: ", HOST, " and PORT is: ", PORT)
     #app.run(HOST, PORT)
-    #app.run(host=None, port=None)
-    #app.run(host='127.0.0.1', port=5555)
-    app.run(host='0.0.0.0', port=8080, debug=True)
 
-    
+
+    # for external (google app engine enviornment):
+    app.run(host='0.0.0.0', port=8080, debug=True)
