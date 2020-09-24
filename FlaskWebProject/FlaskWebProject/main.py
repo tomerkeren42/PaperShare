1307ce8fc04c54e66750f15052fd4baf1036de75
Tomer <tomerkeren42@gmail.com>
2020-09-24 18:01:08 +0300

    temp

--- FlaskWebProject/FlaskWebProject/main.py
+++ FlaskWebProject/FlaskWebProject/main.py
@@ -87,4 +87,4 @@ if __name__ == '__main__':
 
 
     ### for external (google app engine enviornment):
-    # app.run(host='0.0.0.0', port=8080, debug=True)
+    #app.run(host='0.0.0.0', port=8080, debug=True)
